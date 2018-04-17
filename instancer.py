b5b566927ea56ff64fc1ba97aecdb654ff793457
Nikita Akimov <korchiy@yandex.ru>
2018-04-17 18:17:38 +0300

    update

--- instancer.py
+++ instancer.py
@@ -11,7 +11,7 @@ bl_info = {
     'name': 'Instancer',
     'category': 'All',
     'author': 'Nikita Akimov',
-    'version': (1, 0, 0),
+    'version': (1, 0, 1),
     'blender': (2, 79, 0),
     'location': 'The 3D_View window - T-panel - the 1D tab',
     'wiki_url': 'https://github.com/Korchy/1d_instancer',
